b651c86856422de6869fa95f2b4182b99c4e83a3
brunoadller <adller676@gmail.com>
2024-10-30 20:33:17 -0300

    update in helper views pagecontroller and index

--- projeto-canil/backend/src/controllers/pageControllers.ts
+++ projeto-canil/backend/src/controllers/pageControllers.ts
@@ -1,16 +1,61 @@
 import {Request, Response} from 'express'
 
+
 export const home = (req: Request, res: Response) => {
-    res.send("home no controller")
+    res.render("pages/page", {
+        menu:{
+            all: true,
+            dog: false,
+            cat: false,
+            fish: false
+        },
+        banner:{
+            title: 'Todos os animais',
+            backgound: 'allanimals.jpg'
+        }
+    })
     //res.render('pages/page)
 }
 export const dogs = (req: Request, res: Response) => {
-    
+    res.render('pages/page', {
+        menu:{
+            all: false,
+            dog: true,
+            cat: false,
+            fish: false
+        },
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    })
     //res.render('pages/page)
 }
 export const cats = (req: Request, res: Response) => {
-    //res.render('pages/page)
+    res.render('pages/page', {
+        menu:{
+            all: false,
+            dog: false,
+            cat: true,
+            fish: false
+        },
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    })
 }
 export const fishes = (req: Request, res: Response) => {
-    //res.render('pages/page)
+    res.render('pages/page', {
+        menu:{
+            all: false,
+            dog: false,
+            cat: false,
+            fish: true
+        },
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    })
 }
---
+++ projeto-canil/backend/src/helpers/createMenuObjects.ts
@@ -0,0 +1,10 @@
+
+type MenuOptions = 'all' | 'dog' | 'cat' | 'fish'
+export const createMenuObject = (activeMenu: MenuOptions) => {
+    let returnObject = {
+        all: false,
+        dog: false,
+        cat: false,
+        fish: false
+    }
+}
